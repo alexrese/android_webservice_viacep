5b29a27ae668873d779eedfbd65cb74b9999e9a9
alexrese <alexrese@outlook.com>
2019-05-27 15:53:30 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/alexrese/androidwsviacep/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.alexrese.androidwsviacep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String URL = "http://viacep.com.br/ws/01001000/json/";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("ViaCep Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("ViaCep Response", error.toString());
+                    }
+                }
+        );
+        requestQueue.add(objectRequest);
+
+    }
+}
